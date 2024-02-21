07ef23237954ae070698db958a86567fa1eb2ce1
jose veas <joselveas@gmail.com>
2024-02-20 21:51:38 -0300

    modo dark/light

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,3 +1,29 @@
 $(function(){
     $(".navbar").load("menu.html");
+    document.body.setAttribute('theme', 'light');
 })
+
+const btn = document.getElementById("modeToggle");
+const currentTheme = localStorage.getItem('theme');
+
+btn.addEventListener('click', ()=>{
+    setTheme();
+})
+function setTheme(){
+    let currentTheme = document.body.getAttribute('theme');
+    if(currentTheme === 'light'){
+        setDarkMode();
+    }else{
+        setLightMode();
+    }
+}
+function setDarkMode(){
+    document.body.setAttribute('theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    document.getElementById("themeButton").src="./resources/images/iconLight.png";
+};
+function setLightMode(){
+    document.body.setAttribute('theme', 'light');
+    localStorage.setItem('theme', 'light');
+    document.getElementById("themeButton").src="./resources/images/iconDark.png";
+};
